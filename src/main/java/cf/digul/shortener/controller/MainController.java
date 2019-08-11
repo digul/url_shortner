4c1000a7efb390cccb0cb2bdee983cfa799d36c8
digul <ara.digul@gmail.com>
2019-08-11 22:11:05 +0900

    warning정리

--- src/main/java/cf/digul/shortener/controller/MainController.java
+++ src/main/java/cf/digul/shortener/controller/MainController.java
@@ -10,11 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import cf.digul.shortener.service.UrlShortenerService;
 import cf.digul.shortener.vo.Url;
@@ -23,7 +20,6 @@ import cf.digul.shortener.vo.Url;
 @RestController
 public class MainController {
 	private static final Logger logger = LogManager.getLogger(MainController.class);
-	private ObjectMapper mapper = new ObjectMapper();
 	
 	@Autowired
 	private UrlShortenerService urlShortenerService;
--- src/test/java/cf/digul/shortener/controller/MainControllerTest.java
+++ src/test/java/cf/digul/shortener/controller/MainControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
